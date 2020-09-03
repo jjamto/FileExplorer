7c3c1d5d2da6ed2f4112f2a37818afcb70e9a57b
재훈 장 <jangjh@cephasvalve.com>
2020-09-03 19:02:37 +0900

    Completed

--- FileExplorer/Form1.Designer.cs
+++ FileExplorer/Form1.Designer.cs
@@ -40,14 +40,19 @@
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.새로고침ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btFileDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.이름변경ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.복사ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.이동ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prb1 = new System.Windows.Forms.ProgressBar();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 450);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 488);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(646, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(797, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -55,7 +60,7 @@
             // 
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(646, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(797, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -64,7 +69,7 @@
             this.tbPath.Dock = System.Windows.Forms.DockStyle.Top;
             this.tbPath.Location = new System.Drawing.Point(0, 25);
             this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(646, 21);
+            this.tbPath.Size = new System.Drawing.Size(797, 21);
             this.tbPath.TabIndex = 2;
             // 
             // tvPath
@@ -72,12 +77,14 @@
             this.tvPath.Dock = System.Windows.Forms.DockStyle.Left;
             this.tvPath.Location = new System.Drawing.Point(0, 46);
             this.tvPath.Name = "tvPath";
-            this.tvPath.Size = new System.Drawing.Size(194, 404);
+            this.tvPath.Size = new System.Drawing.Size(194, 437);
             this.tvPath.TabIndex = 3;
+            this.tvPath.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvPath_AfterSelect);
             this.tvPath.DoubleClick += new System.EventHandler(this.tvPath_DoubleClick);
             // 
             // lvFiles
             // 
+            this.lvFiles.CheckBoxes = true;
             this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
@@ -88,7 +95,7 @@
             this.lvFiles.HideSelection = false;
             this.lvFiles.Location = new System.Drawing.Point(194, 46);
             this.lvFiles.Name = "lvFiles";
-            this.lvFiles.Size = new System.Drawing.Size(452, 404);
+            this.lvFiles.Size = new System.Drawing.Size(603, 437);
             this.lvFiles.TabIndex = 4;
             this.lvFiles.UseCompatibleStateImageBehavior = false;
             this.lvFiles.View = System.Windows.Forms.View.Details;
@@ -116,9 +123,13 @@
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.새로고침ToolStripMenuItem});
+            this.새로고침ToolStripMenuItem,
+            this.btFileDelete,
+            this.이름변경ToolStripMenuItem,
+            this.복사ToolStripMenuItem,
+            this.이동ToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 114);
             // 
             // 새로고침ToolStripMenuItem
             // 
@@ -127,15 +138,52 @@
             this.새로고침ToolStripMenuItem.Text = "새로고침";
             this.새로고침ToolStripMenuItem.Click += new System.EventHandler(this.새로고침ToolStripMenuItem_Click);
             // 
+            // btFileDelete
+            // 
+            this.btFileDelete.Name = "btFileDelete";
+            this.btFileDelete.Size = new System.Drawing.Size(122, 22);
+            this.btFileDelete.Text = "삭제";
+            this.btFileDelete.Click += new System.EventHandler(this.btFileDelete_Click);
+            // 
+            // 이름변경ToolStripMenuItem
+            // 
+            this.이름변경ToolStripMenuItem.Enabled = false;
+            this.이름변경ToolStripMenuItem.Name = "이름변경ToolStripMenuItem";
+            this.이름변경ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.이름변경ToolStripMenuItem.Text = "이름변경";
+            // 
+            // 복사ToolStripMenuItem
+            // 
+            this.복사ToolStripMenuItem.Enabled = false;
+            this.복사ToolStripMenuItem.Name = "복사ToolStripMenuItem";
+            this.복사ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.복사ToolStripMenuItem.Text = "복사";
+            // 
+            // 이동ToolStripMenuItem
+            // 
+            this.이동ToolStripMenuItem.Enabled = false;
+            this.이동ToolStripMenuItem.Name = "이동ToolStripMenuItem";
+            this.이동ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.이동ToolStripMenuItem.Text = "이동";
+            // 
+            // prb1
+            // 
+            this.prb1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.prb1.Location = new System.Drawing.Point(0, 483);
+            this.prb1.Name = "prb1";
+            this.prb1.Size = new System.Drawing.Size(797, 5);
+            this.prb1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 472);
+            this.ClientSize = new System.Drawing.Size(797, 510);
             this.Controls.Add(this.lvFiles);
             this.Controls.Add(this.tvPath);
             this.Controls.Add(this.tbPath);
             this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.prb1);
             this.Controls.Add(this.statusStrip1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -160,6 +208,11 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 새로고침ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btFileDelete;
+        private System.Windows.Forms.ToolStripMenuItem 이름변경ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 복사ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 이동ToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar prb1;
     }
 }
 
--- FileExplorer/Form1.cs
+++ FileExplorer/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FileExplorer
@@ -35,19 +29,32 @@ namespace FileExplorer
         /// <summary>
         /// 현재 폴더내의 파일목록을 갱신
         /// </summary>
-        void Refresh_file(string path)
+        Boolean Refresh_file(string path)
         {
             lvFiles.Items.Clear(); //기존의 목록을 제거
-            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(tbPath.Text);
-            foreach (var fi in di.GetFiles())
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+
+            try
             {
-                //파일명, 날짜, 확장자, 크기
-                var lv = lvFiles.Items.Add(fi.Name);
-                lv.SubItems.Add(fi.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
-                lv.SubItems.Add(fi.Extension);
-                lv.SubItems.Add(fi.Length.ToString("N0"));
+                foreach (var fi in di.GetFiles())
+                {
+                    //파일명, 날짜, 확장자, 크기
+                    var lv = lvFiles.Items.Add(fi.Name);
+                    lv.SubItems.Add(fi.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    lv.SubItems.Add(fi.Extension);
+                    lv.SubItems.Add(fi.Length.ToString("N0"));
+                    lv.Tag = fi.FullName;
+                }
+                lvFiles.Tag = path;
+                return true;
             }
-            lvFiles.Tag = path;
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+
 
         }
 
@@ -73,14 +80,23 @@ namespace FileExplorer
             {
                 var curpath = node.Tag.ToString();
                 System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(curpath); //D:\
-                foreach (var fi in di.GetDirectories())
+                try
                 {
-                    var lv = node.Nodes.Add(fi.Name); //폴더명
-                    lv.Tag = fi.FullName;
+                    foreach (var fi in di.GetDirectories())
+                    {
+                        var lv = node.Nodes.Add(fi.Name); //폴더명
+                        lv.Tag = fi.FullName;
+                    }
                 }
-           
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+
             }
-            
+
 
         }
 
@@ -92,12 +108,101 @@ namespace FileExplorer
 
         private void tvPath_DoubleClick(object sender, EventArgs e)
         {
+
+        }
+
+        TreeNode preNode = null;
+
+        private void tvPath_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //이전에 선택된 자료의 강조를 제거
+            if (preNode != null)
+            {
+                preNode.ForeColor = this.ForeColor;
+                preNode.NodeFont = this.Font;
+            }
+
             var node = tvPath.SelectedNode; //선택된 노드
             var path = node.Tag.ToString(); //전체경로
             tbPath.Text = path;             // 상단 폴더위치 텍박업데이트
-            Refresh_file(path);             // 실제 파일 목록 갱신
+            var result = Refresh_file(path);             // 실제 파일 목록 갱신
             //하위폴더 조회
-            Refresh_path(node);
+            if (result)
+            {
+                Refresh_path(node);
+            }
+            node.ForeColor = Color.Blue;
+            node.NodeFont = new Font(this.Font.FontFamily, this.Font.Size, FontStyle.Bold);
+
+            preNode = node;
+        }
+
+        private void btFileDelete_Click(object sender, EventArgs e)
+        {
+            //파일삭제
+            if (lvFiles.CheckedItems.Count < 0)
+            {
+
+                int 파일갯수 = lvFiles.CheckedItems.Count;
+                //다중파일삭제루틴
+                var file = new System.IO.FileInfo(lvFiles.FocusedItem.Tag.ToString());
+                string msg = 파일갯수.ToString() + "건의 다음파일을 삭제하시겠습니까?\n";
+
+                var dlg = MessageBox.Show(msg, "파일삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dlg == DialogResult.Yes)
+                {
+                    //진행율 초기화
+                    prb1.Maximum = 파일갯수;
+                    prb1.Minimum = 0;
+                    prb1.Value = 0;
+
+                    foreach (ListViewItem lv in lvFiles.CheckedItems)
+                    {
+                        prb1.Value += 1;
+                        try
+                        {
+                            System.IO.File.Delete(lv.Tag.ToString());  //tag에 파일명 있음
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+
+                        }
+                    }
+
+                    var path = lvFiles.Tag.ToString(); //기존에 조회된 폴더를 찾음
+                    Refresh_file(path);
+
+
+                }
+
+            }
+            else
+            {
+                var file = new System.IO.FileInfo(lvFiles.FocusedItem.Tag.ToString());
+                string msg = "다음파일을 삭제하시겠습니까?\n";
+                msg += "\n파일명 : " + file.Name;
+                msg += "\n경로 : " + file.DirectoryName;
+                msg += "\n크기 : " + file.Length.ToString();
+                var dlg = MessageBox.Show(msg, "파일삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dlg == DialogResult.Yes)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(file.FullName);
+                        var path = lvFiles.Tag.ToString(); //기존에 조회된 폴더를 찾음
+                        Refresh_file(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+
+                    }
+
+
+                }
+
+            }
         }
     }
 }
